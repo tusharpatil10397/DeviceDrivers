69013cbbd6779b88fe296e7522e70b0b7ddd4431
Tushar <h20190097@goa.bits-pilani.ac.in>
2020-01-20 17:55:06 +0530

    first commit

--- demo.c
+++ demo.c
@@ -1,9 +1,18 @@
 #include<linux/init.h>			//Linux Header Files
 #include<linux/module.h>
+#include<linux/moduleparam.h>
+
+static char *mystr ="hello";
+static int myint = 1234;
+
+module_param(mystr,charp,S_IRUGO);
+module_param(myint,int,S_IRUGO);
+
+MODULE_PARM_DESC(myint,"Count of read writes");
 
 int first_module_init(void)
 {
-	printk(KERN_ALERT "Hey World! What's up?\n\n");
+	printk(KERN_ALERT "Hey World! What's up? %d \n\n",myint);
 	return 0;
 }
 
@@ -14,3 +23,8 @@ void first_module_exit(void)
 
 module_init(first_module_init);           //Entry Function
 module_exit(first_module_exit);           //Exit Function
+
+MODULE_DESCRIPTION("Our first module");
+MODULE_AUTHOR("Tushar patil <tusharpatil10397@gmail.com>");
+MODULE_LICENSE("GPL");
+MODULE_INFO(SupportedChips, "PCA9685, FT232RL");
